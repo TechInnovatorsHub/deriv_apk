6be3e9b5babfae7392100c7f8a523db53824f80d
McRonaah <ronaldcheruiyot342@gmail.com>
2024-08-27 16:04:33 +0300

    :tada: project start and installations :tada:

---
+++ deriv/src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Welcome to Deriv</h1>
+        <p>Deriv </p>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
